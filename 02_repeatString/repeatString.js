1bdba8f6ed57dfa052f656d28fd00d2c3598f720
NotsTree <88728626+NotsTree@users.noreply.github.com>
2025-04-03 18:52:42 +1000

    part 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function (word, time) {
+
+    if (time < 0) {
+        return "ERROR";
+    }
+    let string = "";
+    for (let i = 0; i < time; i++) {
+        string += word;
+    }
+    return string;
 
 };
 
