9bbcf00a0b086126bb9ac9eb46564e5bcaa28d75
NotsTree <88728626+NotsTree@users.noreply.github.com>
2025-04-05 23:36:26 +1000

    finishd up to 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+            const isyeardevidebyfour = year % 4 === 0;
+            const iscentury = year % 100 === 0;
+            const isyeardevidebyfourhundred = year % 400 === 0;
+
+
+            if (isyeardevidebyfour && (!iscentury || isyeardevidebyfourhundred)) {
+                return true;
+            } else {
+                return false;
+            }
+            
 
 };
 
